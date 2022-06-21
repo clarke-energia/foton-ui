4d4770124009a5aa00b36af3875672f1f8739bb9
Felipe <26683783+feslima@users.noreply.github.com>
2022-06-21 10:51:15 -0300

    feat: tailwindcss setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,23 @@
-import type { StorybookConfig } from "@storybook/core-common";
+import type { StorybookConfig } from '@storybook/core-common';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-vite",
+    builder: '@storybook/builder-vite',
     disableTelemetry: true,
     enableCrashReports: false,
   },
