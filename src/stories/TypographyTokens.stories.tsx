16a60575eedc9e80e5ae128695c7249d535a2d7b
Felipe Lima <26683783+feslima@users.noreply.github.com>
2022-06-23 18:18:24 -0300

    * feat: typography tokens (#21)
    * feat: typography tokens
    
    * fix: color token bg prefix
    
    * feat: typography display tokens

---
+++ src/stories/TypographyTokens.stories.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { typography } from '../tokens.json';
+
+type FontSize = 'small' | 'medium' | 'large';
+type FontWeights = 'normal' | 'bold' | 'black';
+const WEIGHTS: FontWeights[] = ['normal', 'bold', 'black'];
+
+interface TypographyListProps {
+  type: keyof typeof typography;
+}
+const TypographyList: React.FC<TypographyListProps> = ({ type }) => {
+  const sizes = Object.keys(typography[type]).filter((key) => key !== 'fontFamily') as FontSize[];
+  return (
+    <table className="mx-auto border-black table-auto border-y">
+      <thead>
+        <tr>
+          <th className="text-4xl font-normal capitalize text-start">{type}</th>
+        </tr>
+        <tr className="border-black border-y">
+          <th className="p-2 font-medium text-start">Example</th>
+          <th className="p-2 font-medium text-start">Token</th>
+          <th className="p-2 font-medium text-start">Font Family</th>
+          <th className="p-2 font-medium text-start">Font Size</th>
+          <th className="p-2 font-medium text-start">Line Height</th>
+          <th className="p-2 font-medium text-start">Weight</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sizes.map((size) =>
+          WEIGHTS.map((weight) => (
+            <tr key={`${type}-${size}-${weight}`} className="border-b border-black">
+              <td className={`capitalize font-${weight} text-${type}-${size}`}>{`${type} ${size}`}</td>
+              <td className="font-mono text-pink-50">{`$text-${type}-${size} $font-${weight}`}</td>
+              <td className="text-center">Inter</td>
+              {/* @ts-ignore */}
+              <td className="text-center">{`${typography[type][size].fontSize}px`}</td>
+              {/* @ts-ignore */}
+              <td className="text-center">{`${typography[type][size].lineHeight}px`}</td>
+              <td className="capitalize">{weight}</td>
+            </tr>
+          )),
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+export default {
+  title: 'Design Tokens/Typography',
+  parameters: {
+    options: { showPanel: false },
+    previewTabs: {
+      'storybook/docs/panel': { hidden: true },
+    },
+    viewMode: 'story',
+  },
+};
+
+export const Caption = () => <TypographyList type="caption" />;
+export const Paragraph = () => <TypographyList type="paragraph" />;
+export const Heading = () => <TypographyList type="heading" />;
+export const Display = () => <TypographyList type="display" />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-let tokens = require('./src/tokens.json');
+const defaultTheme = require('tailwindcss/defaultTheme');
+const tokens = require('./src/tokens.json');
 
+// Colors
 const colorsTokens = tokens.colors;
 
 const generateCustomColorsFromTokens = (colorTokens) => {
   const customColors = {};
 
-  for (family in colorTokens) {
+  for (const family in colorTokens) {
     const colorFamily = colorTokens[family];
 
     for (const color in colorFamily) {
@@ -19,17 +21,44 @@ const generateCustomColorsFromTokens = (colorTokens) => {
   return customColors;
 };
 
+// Typography
+const typographyTokens = tokens.typography;
+
+const generateFontSizeTokens = (typographyTokens) => {
+  const fontSize = {};
+  for (const family in typographyTokens) {
+    const typographyFamily = typographyTokens[family];
+    const sizes = Object.keys(typographyFamily).filter((key) => key !== 'fontFamily');
+
+    sizes.forEach((size) => {
+      fontSize[`${family}-${size}`] = [
+        `${typographyFamily[size].fontSize}px`,
+        `${typographyFamily[size].lineHeight}px`,
+      ];
+    });
+  }
+  return fontSize;
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: generateCustomColorsFromTokens(colorsTokens),
+      fontFamily: {
+        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: generateFontSizeTokens(typographyTokens),
     },
   },
   plugins: [],
   /* see: https://tailwindcss.com/docs/content-configuration#class-detection-in-depth
    * and: https://tailwindcss.com/docs/content-configuration#using-regular-expressions
    */
-  safelist: [{ pattern: /bg-(primary|neutral|warning|danger|orange|blue|purple|pink)-.+/ }],
+  safelist: [
+    { pattern: /bg-(primary|neutral|warning|danger|orange|blue|purple|pink)-.+/ },
+    { pattern: /font-(normal|bold|black)/ },
+    { pattern: /text-(caption|paragraph|heading|display)-.+/ },
+  ],
 };
