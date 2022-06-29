b656e4692f60d0590852462c8073a73e78ad2673
Felipe Lima <26683783+feslima@users.noreply.github.com>
2022-06-29 17:04:25 -0300

    feat: select component (#34)
    * fix: checkbox and radiobutton coloring and borders
    
    * feat: select component
    
    * fix: error class when component is disabled

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -7,12 +7,16 @@ const Checkbox: React.FC<CheckboxProps> = ({ label, ...attrs }) => {
   // discard whatever type is set in 'attrs' and fix it into checkbox
   const { type, id, disabled, className, ...rest } = attrs;
   const disabledClass = disabled ? 'text-neutral-40' : 'text-black';
+  const borderClass = disabled ? 'border-neutral-40' : 'border border-black';
+  const colorClass = disabled ? 'text-neutral-40' : 'text-primary-60';
+
+  const dynamicClasses = [borderClass, colorClass].join(' ');
   return (
     <div className="flex items-center">
       <input
         type="checkbox"
         id={id}
-        className="rounded border-black focus:ring-2 focus:ring-offset-2 accent-primary-60 focus:ring-primary-60"
+        className={`rounded focus:ring-2 focus:ring-offset-2 focus:ring-primary-60 ${dynamicClasses}`}
         disabled={disabled}
         {...rest}
       />
